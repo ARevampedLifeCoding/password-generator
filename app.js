f1800082d3681c6710aca543dc3222eea806b0d4
Randy Langston <arevampedlifecoding@gmail.com>
2021-07-13 20:50:42 -0700

    refactored code half way  done

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const randomFunction = {
   number:getNumber,
   specialCharacters:getSpecial,
 }
-let randomUpper
-let randomLower
-let numbers
-let randomSpecial
+
 function  getUpperCase (){
   return String.fromCharCode(Math.floor(Math.random()* 26)+65);
 }
@@ -22,7 +19,7 @@ function  getSpecial(){
   const symbols = "!@#$%^&*()_+=-<>.,/";
   return symbols[Math.floor(Math.random()*symbols.length)];
 }
-// function generatePassword(){
+function generatePassword(){
   let passwordLength = prompt("How many characters would you like your password to be?");
 
 if ( passwordLength < 8 || passwordLength > 128) {
@@ -30,26 +27,13 @@ if ( passwordLength < 8 || passwordLength > 128) {
 }
 
 
-sign = prompt("Would you like to use uppercase?"); 
-sign = prompt("Would you like to use lowercase?");       
-sign = prompt("Would you like to  use numbers?"); 
-sign = prompt("Would  you like to use special characters?"); 
-
-
-// function generatePassword () {
-//   // let randomPassword
-//   let passwordLength = window.prompt("How many characters?");
-//   if (passwordLength < 8 || passwordLength > 128)
-//     window.alert("Number must be between 8 and 128")
-// } 
-// else 
-// {
-   
-//     let lowerCase = window.confirm("Do you want to use lowercase?")
-//     let upperCase = window.confirm("Do you want to use uppercase?")
-//     let number = window.confirm("Do you want to use numbers?")
-//     let specialCharacters =window.confirm("Do you want to use special characters?")
-//   }
+    let lowerCase = window.confirm("Do you want to use lowercase?")
+    let upperCase = window.confirm("Do you want to use uppercase?")
+    let number = window.confirm("Do you want to use numbers?")
+    let specialCharacters =window.confirm("Do you want to use special characters?")
+return;
+  
+}
 
   
 
@@ -66,16 +50,16 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
+// WHEN I click the button to generate a password---------->
+// THEN I am presented with a series of prompts for password criteria--------->
+// WHEN prompted for password criteria----------->
+// THEN I select which criteria to include in the password--------->
+// WHEN prompted for the length of the password----------->
 // THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
+// WHEN asked for character types to include in the password---------->
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
+// WHEN I answer each prompt--------->
+// THEN my input should be validated and at least one character type should be selected-------->
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
